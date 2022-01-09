0c60fe1ba7a999deb98510db36d90d2f09b0b206
Valerii Zhyla <orator9x@gmail.com>
2022-01-09 16:06:12 +0200

    task 2

--- lib/tasks/start.dart
+++ lib/tasks/start.dart
@@ -1,28 +1,53 @@
+import 'dart:async';
+
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import '../utility/number_stream.dart';
+import 'package:earables_app/stream_chart/widgets/stream_line_chart.dart';
 
-class Start extends StatelessWidget {
+class Start extends StatefulWidget {
   const Start({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
+  State<Start> createState() => _StartState();
+}
+
+class _StartState extends State<Start> {
+  final List<FlSpot> flSpots = [];
+  late final StreamSubscription<FlSpot> sub;
+
+  @override
+  void initState() {
+    sub = NumberStream.getSinusStream
+        .map((event) =>
+            FlSpot(DateTime.now().millisecondsSinceEpoch.toDouble(), event))
+        .listen((event) {
+      setState(() {
+        flSpots.add(event);
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: StreamBuilder<int>(
-          stream: NumberStream.getCounterStream,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Text("Counter: ${snapshot.data}");
-            } else {
-              return const Text("No data");
-            }
-          },
-          initialData: 0,
+      body: LineChart(
+        LineChartData(
+          lineBarsData: [
+            LineChartBarData(spots: flSpots),
+          ],
         ),
       ),
     );
