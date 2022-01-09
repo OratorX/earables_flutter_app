1f783f0ad6dc1987fc3a9bbe2b9912e27ff56de3
Valerii Zhyla <orator9x@gmail.com>
2022-01-09 15:38:11 +0200

    task 1

--- lib/tasks/start.dart
+++ lib/tasks/start.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../utility/number_stream.dart';
 
 class Start extends StatelessWidget {
   const Start({Key? key, required this.title}) : super(key: key);
@@ -11,6 +12,19 @@ class Start extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      );
+      body: Center(
+        child: StreamBuilder<int>(
+          stream: NumberStream.getCounterStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text("Counter: ${snapshot.data}");
+            } else {
+              return const Text("No data");
+            }
+          },
+          initialData: 0,
+        ),
+      ),
+    );
   }
 }
